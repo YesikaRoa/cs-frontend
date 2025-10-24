32dd9fcc050793d1bcea44543d29bc48db312084
Yesika Roa <yroa1402@gmail.com>
2025-10-24 18:29:03 -0400

    fix: agregar protocolo HTTP a la baseURL de axios

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const axiosClient = axios.create({
-  baseURL: 'ec2-52-91-8-167.compute-1.amazonaws.com/api',
+  baseURL: 'http://ec2-52-91-8-167.compute-1.amazonaws.com/api',
   headers: {
     'Content-Type': 'application/json',
   },
